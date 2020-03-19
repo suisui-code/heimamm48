26d5f0f73eeac8d0fd964789fcb711d614e90f25
穗穗穗 <826314370@qq.com>
2020-03-20 01:29:38 +0800

    完成项目结构的搭建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+//整个项目的入口文件
+// 导入vue
+import Vue from "vue";
+// 导入Vue 根组件
+import App from "./App.vue";
+// 导入router
+import router from "./router";
+// 导入elementui
+import Element from 'element-ui'
+// 导入 elementui样式(可以官网复制粘贴)
+import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.config.productionTip = false
-
+// 使用element
+Vue.use(Element)
+Vue.config.productionTip = false;
+// 创建了一个新的vue实例
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router: router, //将路由对象挂载到Vue实例中
+  render: h => h(App) //将根组件内容渲染到APP.vue中
+}).$mount("#app");
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+// 导入vue 
+import Vue from 'vue'
+// 导入vue-router
+import VueRouter from 'vue-router'
+// 使用vue-router
+Vue.use(VueRouter)
+// 创建一个新的 vue-router对象
+const router = new VueRouter({
+    // 添加路由规则
+    router:[
+        
+    ]
+})
+// 将新的对象暴露出去
+export default router
